295fce5ed3c2727a103a4538afa5decfce3d57fd
Juan José <juanjo.restrepo97@gmail.com>
2022-02-28 00:11:38 -0500

    comienzo de implementación de envío de correo

--- src/main/java/com/movies/api/repository/ChairRepository.java
+++ src/main/java/com/movies/api/repository/ChairRepository.java
@@ -4,6 +4,7 @@ import com.movies.api.entity.Chair;
 import com.movies.api.entity.Room;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,7 +17,6 @@ public interface ChairRepository  extends JpaRepository<Chair,Long> {
     Optional<Chair> findById(@NotNull Long id);
     @Override
     boolean existsById(@NotNull Long id);
-
     Optional<Chair>findByRowAndColumnAndRoom(String row, int column, Room room);
 
 
